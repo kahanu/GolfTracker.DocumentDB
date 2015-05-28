7995ec6b4bf2889f03a930b3277181961378519c
King Wilder <info@kingwilder.com>
2015-05-28 11:05:39 -0700

    Added a Disposable class for the DocumentDBClient class.

---
+++ GolfTracker.WebApi/Repositories/Disposable.cs
@@ -0,0 +1,36 @@
+﻿using System;
+using System.Linq;
+
+namespace GolfTracker.WebApi.Repositories
+{
+    public abstract class Disposable : IDisposable
+    {
+        private bool isDisposed;
+
+        ~Disposable()
+        {
+            Dispose(false);
+        }
+
+        protected virtual void DisposeCore()
+        {
+
+        }
+
+        private void Dispose(bool disposing)
+        {
+            if (!isDisposed && disposing)
+            {
+                DisposeCore();
+            }
+
+            isDisposed = true;
+        }
+
+        public void Dispose()
+        {
+            Dispose(true);
+            GC.SuppressFinalize(this);
+        }
+    }
+}
