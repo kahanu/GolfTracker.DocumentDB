173238261fde9215ca64c04d186786f0ec5998f2
Mark Hamilton <mark@mark-hamilton.com>
2015-11-11 21:49:37 -0800

    Update homeController.js
    Add variable for userName. Add listener for broadcast event that is handled by indexController (used for Login Form). Also copies over the logOut() function from indexController. This can be used as a model for a navbarController if you decide to split it out.
    
    Part 2 of navbar fix.

--- GolfTracker.Angular/app/home/homeController.js
+++ GolfTracker.Angular/app/home/homeController.js
@@ -1,12 +1,29 @@
-﻿(function () {
+(function () {
     'use strict';
 
     angular.module('golftracker')
-        .controller('homeController', ["authService", "eventAggregator", function (authService, eventAggregator) {
+        .controller('homeController', ["authService", "eventAggregator", "$scope", "$rootScope", function (authService, eventAggregator, $scope, $rootScope) {
             var vm = this;
 
+            vm.userName = authService.authentication.userName;
             vm.isAuthenticated = authService.authentication.isAuth;
+
             console.log("home/vm.isAuthenticated: " + vm.isAuthenticated);
+
+            $scope.$on('user-authenticated', function (evt, data) {
+                console.log("user authenticated called")
+                vm.isAuthenticated = data.isAuthenticated;
+                vm.userName = data.userName;
+            });
+
+            vm.logOut = function () {
+                authService.logOut();
+                //eventAggregator.trigger("isAuthenticated", false);
+                vm.isAuthenticated = false;
+                console.log("home/vm.logOut()/vm.isAuthenticated: " + vm.isAuthenticated);
+
+                $location.path('/home');
+            };
 
         }]);
 })();
