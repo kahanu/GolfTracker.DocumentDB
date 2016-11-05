3f48975d80d3c4382e47ef57bfee923e7f54fe4a
Mark Hamilton <TheDarkCode@users.noreply.github.com>
2016-11-05 07:01:58 -0700

    Update Global.asax.cs
    Add optional "_format=json / xml / bson" query param to override output format for endpoints.
    
    Others to add later: PDF, CSV (for IEnumerable outputs), and YAML.

--- GolfTracker.WebApi/Global.asax.cs
+++ GolfTracker.WebApi/Global.asax.cs
@@ -1,6 +1,8 @@
 ﻿using System;
 using System.Collections.Generic;
 using System.Linq;
+using System.Net.Http.Formatting;
+using System.Net.Http.Headers;
 using System.Web;
 using System.Web.Http;
 using System.Web.Mvc;
@@ -15,6 +17,16 @@ namespace GolfTracker.WebApi
         {
             AreaRegistration.RegisterAllAreas();
             GlobalConfiguration.Configure(WebApiConfig.Register);
+            
+            // Global Formatting Parameter
+            GlobalConfiguration.Configuration.Formatters.JsonFormatter.MediaTypeMappings.Add(new QueryStringMapping("_format", "json", new MediaTypeHeaderValue("application/json")));
+            GlobalConfiguration.Configuration.Formatters.XmlFormatter.MediaTypeMappings.Add(new QueryStringMapping("_format", "xml", new MediaTypeHeaderValue("application/xml")));
+            // BSON Support
+            var bson = new BsonMediaTypeFormatter();
+            bson.SupportedMediaTypes.Add(new MediaTypeHeaderValue("application/bson"));
+            bson.MediaTypeMappings.Add(new QueryStringMapping("_format", "bson", new MediaTypeHeaderValue("application/bson")));
+            GlobalConfiguration.Configuration.Formatters.Add(bson);
+            
             FilterConfig.RegisterGlobalFilters(GlobalFilters.Filters);
             RouteConfig.RegisterRoutes(RouteTable.Routes);
             BundleConfig.RegisterBundles(BundleTable.Bundles);
